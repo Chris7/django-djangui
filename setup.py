d029764f284f820f207875b5580f14a8e68cb449
Chris Mitchell <chris.mit7@gmail.com>
2015-06-26 09:00:43 -0400

    version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-djangui',
-    version='0.2.6',
+    version='0.2.8',
     packages=find_packages(),
     scripts=['scripts/djanguify'],
     entry_points={'console_scripts': ['djanguify = djangui.backend.command_line:bootstrap',]},
